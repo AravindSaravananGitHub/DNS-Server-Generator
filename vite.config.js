ca74a1b90ef6eb8070ef232c64c8b26076a4bb62
Aravind S <aravindprime123@gmail.com>
2025-08-05 09:36:18 +0530

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+const Repo = "DNS-Server-Generator";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: `/` + Repo + `/`,
+});
